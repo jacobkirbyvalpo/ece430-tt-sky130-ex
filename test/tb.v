28decc98659fe3cf04f190a639dcfea5e33e033b
Jacob Kirby <jacob.kirby@valpo.edu>
2026-05-04 03:04:31 +0200

    Add gate-level power pins to testbench

--- test/tb.v
+++ test/tb.v
@@ -3,16 +3,25 @@
 
 module tb;
 
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
+  reg clk = 0;
+  reg rst_n = 1;
+  reg ena = 1;
+  reg [7:0] ui_in = 0;
   wire [7:0] uo_out;
-  reg [7:0] uio_in;
+  reg [7:0] uio_in = 0;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
+
   tt_um_health_monitor_decoder user_project (
+`ifdef GL_TEST
+    .VPWR(VPWR),
+    .VGND(VGND),
+`endif
     .ui_in(ui_in),
     .uo_out(uo_out),
     .uio_in(uio_in),
